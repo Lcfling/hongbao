f01363645d01121a4ba62ebf160d5014bcdb3e94
apple <aylui2009@163.com>
2018-10-11 09:57:29 +0800

    lcfling model update

---
+++ Baocms/Lib/Model/RoomModel.class.php
@@ -0,0 +1,43 @@
+<?php
+class UsersModel extends CommonModel
+{
+    protected $pk = 'id';
+    protected $tableName = 'users';
+
+
+    //获取房间信息
+    public function getroom($roomid)
+    {
+        $data=unserialize(Cac()->get('room_'.$roomid));
+        if(empty($data)){
+            $data=$this->find();
+        }
+    }
+    /*获取房间列表 全部或者某一个类型
+     *@param $type
+     *
+     */
+    public function getroomlist($type=''){
+
+    }
+    //保存信息房间
+    public function saveroom($data = '', $options = array())
+    {
+
+    }
+
+
+    /**更改房间信息 房间号
+     * @param $id
+     */
+    public function updateroom($id){
+
+    }
+
+    /**更新房间列表的缓存
+     * 
+     */
+    public function updateroomlist(){
+
+    }
+}
--- Baocms/Lib/Model/UsersModel.class.php
+++ Baocms/Lib/Model/UsersModel.class.php
@@ -3,7 +3,7 @@ class UsersModel extends CommonModel
 {
     protected $pk = 'user_id';
     protected $tableName = 'users';
-    protected $_integral_type = array('share' => '发帖分享', 'reply' => '回复帖子', 'mobile' => '手机认证', 'email' => '邮件认证');
+    //protected $_integral_type = array('share' => '发帖分享', 'reply' => '回复帖子', 'mobile' => '手机认证', 'email' => '邮件认证');
     public function checkInvite($uid, $money)
     {
         //推广员功能
@@ -30,7 +30,27 @@ class UsersModel extends CommonModel
         }
         return false;
     }
-    public function getUserByAccount($account){
+    public function getUserByUid($user_id,$cleanCache=false){
+        if($cleanCache){
+            Cac()->set('userinfo_'.$user_id,null);
+            $data = $this->find(array('where' => array('user_id' => $user_id)));
+        }else{
+            $data=Cac()->get('userinfo_'.$user_id);
+            if($data!=null){
+                $data=unserialize($data);
+            }
+            if(!empty($data)){
+                return $this->_format($data);
+            }else{
+                $data = $this->find(array('where' => array('user_id' => $user_id)));
+                Cac()->set('userinfo_'.$user_id,serialize($data));
+            }
+        }
+
+        //$data = $this->find(array('where' => array('account' => $account)));
+        return $this->_format($data);
+    }
+    public function getUserByAccount($account,$cleanCache=false){
         $data = $this->find(array('where' => array('account' => $account)));
         return $this->_format($data);
     }
