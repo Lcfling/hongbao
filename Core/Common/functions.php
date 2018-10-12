6c92dccf1e7ea25c39d4acf51f2c27de9ce859de
apple <aylui2009@163.com>
2018-10-12 14:50:49 +0800

    lcfling update core functions

--- Core/Common/functions.php
+++ Core/Common/functions.php
@@ -838,3 +838,22 @@ function think_filter(&$value){
         $value .= ' ';
     }
 }
+//随机函数
+function genRandomString($len = 6) {
+    $chars = array(
+        "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
+        "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
+        "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G",
+        "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R",
+        "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2",
+        "3", "4", "5", "6", "7", "8", "9"
+    );
+    $charsLen = count($chars) - 1;
+// 将数组打乱
+    shuffle($chars);
+    $output = "";
+    for ($i = 0; $i < $len; $i++) {
+        $output .= $chars[mt_rand(0, $charsLen)];
+    }
+    return $output;
+}
